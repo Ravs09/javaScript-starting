56b0f6f7600cc0c0c3a48e1284ca6870af1b2fbb
Ravs09 <ravs670@gmail.com>
2026-09-06 16:44:20 +0600

    sff

---
+++ array_of_object.js
@@ -0,0 +1,8 @@
+const student = [
+    { id: 1, name: "Sakib", age: 19, marks: 33 },
+    { id: 2, name: "Rakib", age: 15, marks: 30 },
+    { id: 3, name: "Sobuj", age: 22, marks: 79 },
+    { id: 4, name: "Raj", age: 24, marks: 65 },
+    { id: 5, name: "Fahim", age: 22, marks: 55 }
+];
+console.log(student)
